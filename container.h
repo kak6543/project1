28071ac4a161aab66103dfdfacbbca282c7b82cd
cjn5475 <cjn5475@psu.edu>
2024-10-15 15:43:12 -0400

    Update container.h

---
+++ container.h
@@ -0,0 +1,8 @@
+#ifndef CONTAINER_H
+#define CONTAINER_H
+struct FlightInfo {
+    const char* destination;  // store and reference airport codes
+    int distance;             // miles to destination
+};
+
+#endif
